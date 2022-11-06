876c46384a20bc7bc009e9e46705e0a0f60c1ced
karim <akskarim17@gmail.com>
2022-11-07 02:35:11 +0600

    Files added

---
+++ README.md
@@ -0,0 +1,142 @@
+# Stealth Seminar API Registration limit
+
+This project control how many times a user can register for a webinar using a email or a IP address or both. This api endpoint is works like a middleman. 
+## Features
+
+- Limit registration by email address
+- Limit registration by IP address
+- Client's IP in custom filed added automatically
+
+
+
+## Usage
+#### Add file to server
+Make a new folder eg: webinar, then add these file inside. 
+```
+server_root
+│    ...    
+│
+└─── webinar
+    │   limit.php
+    │   user.json
+```
+now your api endpoint is: `https://your-domain/webinar/limit.php`
+
+#### Change url in your api form
+Here I'm using form from [this project](https://github.com/abkarim/stealthseminar-api-registration-form). Just change submit url located in app.js.
+```javascript
+// ...
+axios
+    .post(`https://api.joinnow.live/webinars/${webinarShortId}/registration`, data, {
+        contentType: 'application/json'
+    })
+// ...
+```
+**to** 
+```javascript
+// ...
+axios
+    .post(`https://your-domain/webinar/limit.php`, data, {
+        contentType: 'application/json'
+    })
+// ...
+```
+#### Add webinar short id 
+add your webinar short id in **limit.php**
+```php
+<?php
+//...
+$webinarShortId = ""; // Webinar short id
+```
+
+#### Enable/Disable email count
+```php
+<?php
+//...
+$validateWithEMail = true; // email duplication handling
+```
+
+#### Enable/Disable IP count
+```php
+<?php
+//...
+$validateWithIP = false; // IP address duplication handling
+```
+
+#### Change maximum registration count
+```php
+<?php
+//...
+$validateWithIP = false; // IP address duplication handling
+```
+
+#### Change maximum number of email duplication response
+```php
+<?php
+// ...
+echo json_encode([
+                    "message" => "you are already registered in this event, please check your email!",
+                ]); // Email matched message
+// ...
+```
+
+#### Change maximum number of IP duplication response
+```php
+<?php
+// ...
+echo json_encode([
+                    "message" => "you are already registered in this event, please check your email!",
+                ]); // IP matched message
+// ...
+```
+
+#### Change custom field name of ip_address
+change ip_address to something else on both condition.
+```php
+<?php
+// ...
+# Add ip address to custom filed
+if (isset($data->customFields)) {
+    $data->customFields->ip_address = $ip;
+} else {
+    $data->customFields = [
+        "ip_address" => $ip,
+    ];
+}
+// ...
+```
+
+#### Clear users list 
+When you want to clear users list or using with different webinar, replace **user.json** with this
+```json
+[]
+```
+
+### How does this project work
+```mermaid
+graph LR
+A[Registration form]
+B(limit.php)
+C(Get required data)
+D[Check ip and email]
+E[Check registration limit]
+F[Append request ip in custom filed]
+G[StealthSeminar api]
+H((Condition))
+I(Return response)
+J[Update user data]
+K[Duplication error message]
+
+A -- POST --> B
+B --> C
+B --> D
+B --> E
+D --> H
+C --> H
+E --> H
+H -- valid --> F
+H -- invalid --> K
+F -- POST --> G
+G -- Get data --> I
+I -- user.json --> J
+```
---
+++ limit.php
@@ -0,0 +1,124 @@
+<?php
+ignore_user_abort(true);
+header("Cache-Control: no-cache");
+header("content-type: application/json");
+header("Access-Control-Allow-Methods:  OPTIONS, POST");
+header("Access-Control-Allow-Origin: '*'"); // Origin
+
+$webinarShortId           = ""; // Webinar short id
+$validateWithEMail        = true; // email duplication handling
+$validateWithIP           = false; // IP address duplication handling
+$maximumRegistrationCount = 1; // Maximum registration
+
+# Handle preflight request
+if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
+    http_response_code(204);
+    exit;
+}
+
+# Handle request
+if ('POST' == $_SERVER['REQUEST_METHOD']) {
+    # Get ip address
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    # Check content type
+    if (!isset($_SERVER['CONTENT_TYPE']) || 'application/json' != $_SERVER['CONTENT_TYPE']) {
+        http_response_code(412);
+        echo json_encode("content-type is not set to application/json");
+        exit;
+    }
+
+    # Get user data
+    $users = json_decode(file_get_contents(__DIR__ . '/user.json'));
+
+    # Get request data
+    $raw_data = file_get_contents("php://input");
+
+    if (!$data = json_decode($raw_data)) {
+        http_response_code(422);
+        echo json_encode("registration data is not valid json");
+        exit;
+    }
+
+    # User email
+    $email     = $data->email;
+    $duplicate = false;
+
+    # is email exists
+    foreach ($users as $key => $user) {
+        # Check email
+        if ($validateWithEMail && $email === $user->email) {
+            # Is maximum count exceed
+            if ($maximumRegistrationCount < $user->count + 1) {
+                http_response_code(406);
+                echo json_encode([
+                    "message" => "you are already registered in this event, please check your email!",
+                ]); // Email matched message
+                exit;
+            }
+            $duplicate = true;
+        }
+
+        # Check ip
+        if ($validateWithIP && $ip === $user->ip) {
+            # Is maximum count exceed
+            if ($maximumRegistrationCount < $user->count + 1) {
+                http_response_code(406);
+                echo json_encode([
+                    "message" => "you are already registered in this event, please check your email!",
+                ]); // IP matched message
+                exit;
+            }
+            $duplicate = true;
+        }
+
+        # Update count
+        if ($duplicate) {
+            $users[$key]->count = $user->count + 1;
+        }
+    }
+
+    # Add ip address to custom filed
+    if (isset($data->customFields)) {
+        $data->customFields->ip_address = $ip;
+    } else {
+        $data->customFields = [
+            "ip_address" => $ip,
+        ];
+    }
+
+    # Post data to StealthSeminar
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, "https://api.joinnow.live/webinars/$webinarShortId/registration");
+    curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl_handle, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl_handle, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen(json_encode($data)),
+    ]);
+    $response  = curl_exec($curl_handle);
+    $http_code = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+    curl_close($curl_handle);
+
+    # Return data to user
+    http_response_code($http_code);
+    echo $response;
+
+    # Add to user list
+    if (!$duplicate) {
+        array_push($users, [
+            "email" => $email,
+            "ip"    => $ip,
+            "time"  => time(),
+            "count" => 1,
+        ]);
+    }
+    if ($http_code === 200) {
+        file_put_contents(__DIR__ . '/user.json', json_encode($users));
+    }
+    exit;
+}
+
+# Default response
+http_response_code(405);
